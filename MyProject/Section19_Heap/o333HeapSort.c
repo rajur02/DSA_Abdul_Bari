5fd267f660468969000af61c778789e3d96a4acd
webdesign4everyonebyme <webdesign4everyonebyme@github.com>
2025-11-01 12:59:04 +0530

    Binary Search Trees, AVL Trees and Heap

---
+++ MyProject/Section19_Heap/o333HeapSort.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Insert(int A[], int n) {
+
+    int i=n, temp;
+    temp = A[i];
+
+    while(i>1 && temp>A[i/2]) {
+        A[i]=A[i/2];
+        i=i/2;
+    }
+    A[i]=temp;
+}
+
+
+//heap sort
+int Delete(int A[], int n) {
+
+    int i, j, x, temp, val;
+    val = A[1];
+    x=A[n];
+    A[1]=A[n];
+    A[n] = val;  //Deleted element stored at last
+    i=1; j=i*2;
+
+    while(j < n-1) {
+        if(A[j+1]>A[j])
+            j=j+1;
+        if(A[i] < A[j]) {
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            i=j;
+            j=2*j;
+        }
+        else
+            break;
+    }
+    return val;
+}
+
+int main() {
+    int H[] = {0,10,20,30,25,5,40,35};  //40,25,35,10,5,20,30
+
+    int i;
+    for(i=2; i<=7; i++) {
+        Insert(H, i);
+    }
+
+    for(i=7; i>1; i--) {
+        Delete(H, i);
+    }
+
+    // printf("Deleted value is: %d\n", Delete(H,7));
+    // printf("Deleted value is: %d\n", Delete(H,6));
+
+    for(i=1; i<=7; i++) {
+        printf("%d ", H[i]);
+    }
+    printf("\n");
+    // Insert(H, 2);
+    // Insert(H, 3);
+    // Insert(H, 4);
+    // Insert(H, 5);
+
+}
